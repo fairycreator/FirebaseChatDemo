fa65b5f9619269d1144c410f18a568f1fc2c9d69
fairycreator <122693095+fairycreator@users.noreply.github.com>
2023-10-27 16:37:42 +0200

    routes fixed

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import {SafeAreaView, StatusBar, View, StyleSheet} from 'react-native';
-import ChatScreen from './screens/chatScreen';
-import auth from '@react-native-firebase/auth';
-import LoginScreen from './screens/loginScreen';
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, StatusBar, View, StyleSheet } from "react-native";
+import ChatScreen from "./screens/ChatScreen";
+import auth from "@react-native-firebase/auth";
+import LoginScreen from "./screens/loginScreen";
 
 const App = () => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     // Auth listener
-    const unsubscribe = auth().onAuthStateChanged(user => {
+    const unsubscribe = auth().onAuthStateChanged((user) => {
       user ? setUser(user) : setUser(null);
     });
     return () => {
@@ -19,7 +19,7 @@ const App = () => {
 
   return (
     <SafeAreaView style={styles.backgroundStyle}>
-      <StatusBar barStyle={'dark-content'} />
+      <StatusBar barStyle={"dark-content"} />
       <View style={styles.viewStyle}>
         {user ? <ChatScreen /> : <LoginScreen />}
       </View>
--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,36 +1,36 @@
 // ChatScreen.js
-import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, ActivityIndicator, Alert } from 'react-native';
-import auth from '@react-native-firebase/auth';
-import firestore from '@react-native-firebase/firestore';
-import SignOutButton from './components/SignOutButton';
-import Input from './components/Input';
-import SendButton from './components/SendButton';
-import Chat from './components/Chat';
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, ActivityIndicator, Alert } from "react-native";
+import auth from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
+import SignOutButton from "../components/SignOutButton";
+import Input from "../components/Input";
+import SendButton from "../components/SendButton";
+import Chat from "../components/Chat";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   textContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   text: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
   inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   chatStyle: {
     flex: 1,
-    width: '100%',
+    width: "100%",
   },
   googleButton: {
     width: 200,
@@ -42,30 +42,33 @@ const handleSignOut = (navigation) => {
   auth()
     .signOut()
     .then(() => {
-      console.log('User signed out!');
-      navigation.navigate('LoginScreen');
-      Alert.alert('Success', 'You have been signed out.');
+      console.log("User signed out!");
+      navigation.navigate("LoginScreen");
+      Alert.alert("Success", "You have been signed out.");
     })
-    .catch(error => {
-      console.error('Error signing out: ', error);
-      Alert.alert('Error', 'There was a problem signing out. Please try again.');
+    .catch((error) => {
+      console.error("Error signing out: ", error);
+      Alert.alert(
+        "Error",
+        "There was a problem signing out. Please try again."
+      );
     });
 };
 
 const ChatScreen = ({ navigation }) => {
   const [chats, setChats] = useState([]);
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [loading, setLoading] = useState(true);
   const timestamp = firestore.FieldValue.serverTimestamp();
 
   useEffect(() => {
     const unsubscribe = firestore()
-      .collection('chats')
-      .orderBy('createdAt', 'asc')
+      .collection("chats")
+      .orderBy("createdAt", "asc")
       .limitToLast(15)
-      .onSnapshot(querySnapshot => {
+      .onSnapshot((querySnapshot) => {
         const chatsArr = [];
-        querySnapshot.forEach(doc => {
+        querySnapshot.forEach((doc) => {
           const id = doc.id;
           const data = doc.data();
           chatsArr.push({ id, ...data });
@@ -89,7 +92,7 @@ const ChatScreen = ({ navigation }) => {
         setLoading(true);
 
         await firestore()
-          .collection('chats')
+          .collection("chats")
           .doc()
           .set({
             owner: uid,
@@ -98,20 +101,20 @@ const ChatScreen = ({ navigation }) => {
             createdAt: timestamp,
           })
           .then(() => {
-            setText('');
+            setText("");
             setLoading(false);
           })
-          .catch(err => {
+          .catch((err) => {
             setLoading(false);
-            Alert.alert('Error', err);
+            Alert.alert("Error", err);
           });
       } catch (err) {
         setLoading(false);
-        Alert.alert('Error', err);
+        Alert.alert("Error", err);
       }
     } else {
       setLoading(false);
-      Alert.alert('Chat not sent', 'Must be between 1 and 40 characters');
+      Alert.alert("Chat not sent", "Must be between 1 and 40 characters");
     }
   };
 
